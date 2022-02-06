945093abe6cb26d79af8a5ad060056d116fa6c3b
CaioProg <caioprog18@gmail.com>
2022-02-06 20:55:39 -0300

    Arrays

--- Study_Material/Loops/Loops.rb
+++ Study_Material/Loops/Loops.rb
@@ -38,3 +38,11 @@ for i in 0..5
 5.times do
     puts "Hello, world!"
 end  
+
+5.times do |number|
+    puts "Alternative fact number #{number}"
+end
+
+5.upto(10) {|num| print "#{num} " }     #=> 5 6 7 8 9 10
+
+10.downto(5) {|num| print "#{num} " }   #=> 10 9 8 7 6 5
