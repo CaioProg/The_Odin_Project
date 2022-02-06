58d39581b62102d178a94f96eb4ad4437a73502a
CaioProg <caioprog18@gmail.com>
2022-02-06 15:16:36 -0300

    Conditional Logic

--- Study_Material/Variables/Variables.rb
+++ Study_Material/Variables/Variables.rb
@@ -5,3 +5,31 @@ age = 18
 age #=> 18
 age = 33
 age #=> 33
+age = 18
+age #=> 18
+age = age + 4
+age #=> 22
+
+age = 18
+age -= 2  #=> 16
+
+cash = 10
+cash *= 2 #=> 20
+
+temperature = 40
+temperature /= 10 #=> 4
+
+# bad
+a = 19
+string = "John"
+
+# good
+age = 19
+name = "John"
+can_swim = false
+
+desired_location = "Barcelona"
+johns_location = desired_location
+
+desired_location  #=> "Barcelona"
+johns_location    #=> "Barcelona"
